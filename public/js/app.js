46098f0e8a498cea707c687a4313742d78498809
Azoraqua <me@azoraqua.com>
2020-02-15 00:20:32 +0100

    Updated.

--- public/js/app.js
+++ public/js/app.js
@@ -10456,6 +10456,8 @@
             }
 
 
+
+
 // Multifunctional method to get and set values of a collection
 // The value/s can optionally be executed if it's a function
             var access = function (elems, fn, key, value, chainable, emptyGet, raw) {
@@ -14877,6 +14879,8 @@
             });
 
 
+
+
 // Return jQuery for attributes-only inclusion
 
 
@@ -16362,6 +16366,8 @@
             });
 
 
+
+
 // Prevent auto-execution of scripts when no explicit dataType was provided (See gh-2432)
             jQuery.ajaxPrefilter(function (s) {
                 if (s.crossDomain) {
@@ -16521,6 +16527,8 @@
             });
 
 
+
+
 // Support: Safari 8 only
 // In Safari 8 documents created via document.implementation.createHTMLDocument
 // collapse sibling forms: the second one becomes a child of the first one.
@@ -16646,6 +16654,8 @@
 
                 return this;
             };
+
+
 
 
 // Attach a bunch of functions for handling common AJAX events
@@ -17032,6 +17042,8 @@
                     // subtraction forces infinities to NaN
                     !isNaN(obj - parseFloat(obj));
             };
+
+
 
 
 // Register as a named AMD module, since jQuery can be concatenated with other
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,23 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Dashboard</div>
+    <style>
+        aside li:hover,
+        aside li:focus {
+            background: rgba(0, 0, 0, 0.075);
+            color: #000;
+        }
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
+        aside .nav-item {
+            padding: 10px;
+        }
+    </style>
 
-                        You are logged in!
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <aside class="navbar navbar-expand-lg bg-white" style="position: absolute; top: 20px; bottom: 0; width: 200px;">
+        <ul class="navbar-nav nav-pills flex-column" style="width: 100%; text-align: center;">
+            <li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-gear"></i> Settings</a></li>
+            <li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-user"></i> Users</a></li>
+            <li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-pencil"></i> Posts</a></li>
+            <li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-hashtag"></i> Tags</a></li>
+        </ul>
+    </aside>
 @endsection
+
